3a7758126f881f8a19b4839477aea2ddefdd7fe5
henrybirch <hab.b@hotmail.co.uk>
2023-03-04 21:49:40 +0000

    Add allLines

--- src/game.ts
+++ src/game.ts
@@ -4,8 +4,8 @@ enum Marker {
 }
 
 type Position = Marker | null;
-type Coordinates = { x: 0 | 1 | 2; y: 0 | 1 | 2 };
-type Line = [Coordinates, Coordinates, Coordinates];
+type Square = { x: 0 | 1 | 2; y: 0 | 1 | 2 };
+type Line = [Square, Square, Square];
 
 type Game = [
   [Position, Position, Position],
@@ -27,7 +27,7 @@ enum gameOutcome {
   Draw,
 }
 
-const game = (game: Game) => {
+const gameLogic = function (game: Game) {
   const leftToRightDiagonal: Line = [
     { x: 0, y: 0 },
     { x: 1, y: 1 },
@@ -38,5 +38,24 @@ const game = (game: Game) => {
     { x: 1, y: 1 },
     { x: 0, y: 2 },
   ];
-  const allLines = () => {};
+  const allLines: Array<Line> = (function () {
+    const lines: Array<Line> = [];
+    const axis: Array<0 | 1 | 2> = [0, 1, 2];
+    for (const coordinate of axis) {
+      lines.push([
+        { x: 0, y: coordinate },
+        { x: 1, y: coordinate },
+        { x: 2, y: coordinate },
+      ]);
+      lines.push([
+        { x: coordinate, y: 0 },
+        { x: coordinate, y: 1 },
+        { x: coordinate, y: 2 },
+      ]);
+    }
+    return lines;
+  })();
+  return { allLines };
 };
+
+console.log(gameLogic(getFreshGame()).allLines);
