f5c85dbbcb91fa42a11829aeffb01b59ac27e2dd
henrybirch <hab.b@hotmail.co.uk>
2023-03-04 15:21:01 +0000

    Add game-logic.ts

---
+++ src/game-logic.ts
@@ -0,0 +1,11 @@
+import { Game } from "./types";
+
+function getNewGame(): Game {
+  return [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null],
+  ];
+}
+
+const gameLogic = (game: Game) => {};
