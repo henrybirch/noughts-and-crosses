37335612183f496c9d9121e67165cba57fd1b414
henrybirch <hab.b@hotmail.co.uk>
2023-03-06 19:31:32 +0000

    Add Turn to square.ts

--- src/square.ts
+++ src/square.ts
@@ -9,9 +9,12 @@ export interface UnoccupiedSquareProps {
   isOccupied: false;
 }
 
+export type Turn = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
+
 export interface OccupiedSquareProps {
   isOccupied: true;
   marker: Marker;
+  turn: Turn;
 }
 
 export const UnoccupiedSquare: Record<UnoccupiedSquareProps> = (() => {
@@ -24,6 +27,7 @@ export const OccupiedSquare: Record.Factory<OccupiedSquareProps> = (() => {
   const anOccupiedSquareProps: OccupiedSquareProps = {
     isOccupied: true,
     marker: Marker.Nought,
+    turn: 0,
   };
   return Record(anOccupiedSquareProps);
 })();
