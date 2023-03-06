b5d663f2d6f36454db0385e9c56633875cfcba05
henrybirch <hab.b@hotmail.co.uk>
2023-03-06 15:17:38 +0000

    add MakeNewGame

--- src/make-new-game.ts
+++ src/make-new-game.ts
@@ -1,6 +1,6 @@
 import { anUnoccupiedSquare, GameBoard } from "./interfaces";
 
-function makeNewGame(): GameBoard {
+export function makeNewGame(): GameBoard {
   return [
     [anUnoccupiedSquare, anUnoccupiedSquare, anUnoccupiedSquare],
     [anUnoccupiedSquare, anUnoccupiedSquare, anUnoccupiedSquare],
