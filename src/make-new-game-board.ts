16dcc46f26c2fce37e261800834766ae331b5e88
henrybirch <hab.b@hotmail.co.uk>
2023-03-06 16:32:02 +0000

    Delete all src

--- src/make-new-game-board.ts
+++
@@ -1,9 +0,0 @@
-import { anUnoccupiedSquare, GameBoard } from "./interfaces";
-
-export function makeNewGameBoard(): GameBoard {
-  return [
-    [anUnoccupiedSquare, anUnoccupiedSquare, anUnoccupiedSquare],
-    [anUnoccupiedSquare, anUnoccupiedSquare, anUnoccupiedSquare],
-    [anUnoccupiedSquare, anUnoccupiedSquare, anUnoccupiedSquare],
-  ];
-}
