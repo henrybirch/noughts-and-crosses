fe1b43c188f6977f665c24fe7f8e8755fa812feb
henrybirch <hab.b@hotmail.co.uk>
2023-03-06 21:16:45 +0000

    Change Coordinates to getCoordinates

--- src/coordinates.ts
+++ src/coordinates.ts
@@ -2,7 +2,7 @@ import { Record } from "immutable";
 
 export type Coordinate = 0 | 1 | 2;
 export type CoordinatesProps = { x: Coordinate; y: Coordinate };
-export const Coordinates: Record.Factory<CoordinatesProps> = (function () {
+export const getCoordinates: Record.Factory<CoordinatesProps> = (function () {
   const someCoordinatesProps: CoordinatesProps = { x: 0, y: 0 };
   return Record(someCoordinatesProps);
 })();
