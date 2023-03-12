068a17e7189b22fc43c91d4eb714113189486963
henrybirch <hab.b@hotmail.co.uk>
2023-03-12 16:53:02 +0000

    Add threeArrayMap

--- src/noughts-and-crosses.ts
+++ src/noughts-and-crosses.ts
@@ -5,6 +5,19 @@ export type Square = Marker | null;
 
 type ThreeArray<A> = readonly [A, A, A];
 
+function threeArrayMap<A>(
+  a: ThreeArray<A>
+): <B>(f: ((v: A, i: Coordinate) => B) | ((v: A) => B)) => ThreeArray<B> {
+  function returnFunction<B>(
+    f: ((v: A, i: Coordinate) => B) | ((v: A) => B)
+  ): ThreeArray<B> {
+    const g = (x: Coordinate) => f(a[x], x);
+    return [g(0), g(1), g(2)];
+  }
+
+  return returnFunction;
+}
+
 export type Row = ThreeArray<Square>;
 export type Board = ThreeArray<Row>;
 
