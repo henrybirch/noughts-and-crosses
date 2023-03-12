744ae7e503a6ceb0322703b4a36a16fdeb3fb2d5
henrybirch <hab.b@hotmail.co.uk>
2023-03-12 15:33:48 +0000

    Add types

---
+++ src/noughts-and-crosses.ts
@@ -0,0 +1,9 @@
+export type Coordinate = 0 | 1 | 2;
+export type Coordinates = { readonly y: Coordinate; readonly x: Coordinate };
+export type Marker = "O" | "X";
+export type Square = Marker | null;
+
+type ThreeArray<A> = readonly [A, A, A];
+
+export type Row = ThreeArray<Square>;
+export type Board = ThreeArray<Row>;
